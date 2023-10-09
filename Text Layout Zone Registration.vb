3ca23f0577ed02741c9be90a0aa037e4b4c91efc
KofaxTransformation <103566874+KofaxTransformation@users.noreply.github.com>
2023-10-09 11:41:46 +0200

    Don't crash if test doc longer than sample doc

--- Text Layout Zone Registration.vb
+++ Text Layout Zone Registration.vb
@@ -40,7 +40,7 @@ Private Sub SL_CalculatePageShift_LocateAlternatives(ByVal pXDoc As CASCADELib.C
          ZonesExist(AZLDef.Zones(Z).PageNr)=True
       Next
    For I=0 To pXDoc.Pages.Count - 1
-      if ZonesExist(I) and pXdoc.Pages.Count <= AZLSampleDoc.Pages.Count then
+      if I < AZLSampleDoc.Pages.Count AndAlso ZonesExist(I) then
          Pages_Compare(AZLSampleDoc.Pages(I),pXDoc.Pages(I),pLocator.Alternatives,pXDoc.CDoc.Pages(I).XRes,pXDoc.CDoc.Pages(I).YRes)
       else
          pLocator.Alternatives.Add.Confidence=1.00
